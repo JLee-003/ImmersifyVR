1f5c9a428ef0474053813e1fbf48315c88876915
codemaster-jojo <jonny.masterpi@gmail.com>
2024-08-31 15:16:45 -0700

    3rd scene

--- Assets/_Jonny/SceneLoaderFolder/SphereScript.cs
+++ Assets/_Jonny/SceneLoaderFolder/SphereScript.cs
@@ -7,8 +7,15 @@ public class SphereScript : MonoBehaviour
     // Start is called before the first frame update
     public void LoadSceneOnGrab()
     {
-        SceneLoader.Instance.LoadNewScene("Scene Loader");
+        SceneLoader.Instance.LoadNewScene("ThirdScene");
+        Debug.Log("Grabbing!");
     }
+
+    public void ColorChangeOnHover()
+    {
+        GetComponent<Renderer>().material.color = new Color(0, 204, 102);
+    }
+
 
     // Update is called once per frame
     void Update()
