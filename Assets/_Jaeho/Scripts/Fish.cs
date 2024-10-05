e09a006cdd12e21b29c81afb9e9d0b0b4050fd4f
JLee-003 <jaeho5540@gmail.com>
2024-10-05 08:55:57 -0700

    Changed fish collection + experimented with breaststroke button

--- Assets/_Jaeho/Scripts/Fish.cs
+++ Assets/_Jaeho/Scripts/Fish.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     public int type;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Controller"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
