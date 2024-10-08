bfc4bf770dd93d7936915d54e4bd8ec7780c1874
codemaster-jojo <jonny.masterpi@gmail.com>
2024-10-07 19:01:18 -0700

    teleport and money singletons

--- Assets/_Jonny/Environment/Money.cs
+++ Assets/_Jonny/Environment/Money.cs
@@ -8,10 +8,20 @@ public class Money : MonoBehaviour
     public int money = 1000;
     public TextMeshProUGUI moneyText;
     // Start is called before the first frame update
-    void Start()
+    public static Money Instance { get; private set; }
+    private void Awake()
     {
-        
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
+
 
     public void AddMoney(int amount) {
         money += amount;
--- Assets/_Jonny/Teleport.cs
+++ Assets/_Jonny/Teleport.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    public static Teleport Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    
     public void tp(int x, int y, int z) {
         transform.position = new Vector3(x, y, z);
     }
 
+    public void tpToSpecificPos() {
+        tp(100,20,100);
+    }
+
     void Update() {
-        tp(100,100,100);
     }
 }
  
