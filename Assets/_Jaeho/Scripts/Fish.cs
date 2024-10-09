05404c7c0096d3cc4ee61d2ed0ee2d31c8a5fc85
JLee-003 <jaeho5540@gmail.com>
2024-10-08 20:59:10 -0700

    Added models + made different fish
    Random fish now spawn with their type model/value

--- Assets/_Jaeho/Scripts/Fish.cs
+++ Assets/_Jaeho/Scripts/Fish.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    MeshFilter meshFilter;
+    //Renderer objRenderer;
+    public Mesh mesh1, mesh2;
+    public int value1, value2;
+    //public Material mat1, mat2;
+
     public int type;
-    int value = 100;
+
+    int value;
+
+    private void Start()
+    {
+        meshFilter = GetComponentInChildren<MeshFilter>();
+        //objRenderer = GetComponent<Renderer>();
+        SetModel();
+    }
+
+    void SetModel()
+    {
+        switch (type)
+        {
+            case 1: meshFilter.mesh = mesh1; value = value1; break;
+            case 2: meshFilter.mesh = mesh2; value = value2; break;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Controller"))
