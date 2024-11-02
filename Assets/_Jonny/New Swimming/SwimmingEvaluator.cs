01b04dfaf541ab18c6ce567bcc51d3d7a98fe50f
codemaster-jojo <jonny.masterpi@gmail.com>
2024-11-02 12:41:07 -0700

    fixed some bugs

--- Assets/_Jonny/New Swimming/SwimmingEvaluator.cs
+++ Assets/_Jonny/New Swimming/SwimmingEvaluator.cs
@@ -26,12 +26,16 @@ public class SwimmingEvaluator : MonoBehaviour
 
     float decelerationFactor = 0.98f;
 
+    float speedFactor;
+
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = head.GetComponent<CharacterController>();
         controller = GetComponent<ActionBasedController>();
+        speedFactor = 0;
+    }
 
     // Update is called once per frame
     void Update()
@@ -41,38 +45,39 @@ public class SwimmingEvaluator : MonoBehaviour
 
         // Conditional: if controller is moving above X speed, apply boost using acceleration
         if (true) {
-            ApplyBoost()
+            ApplyBoost();
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        currentSpeed *= decelerationFactor;
-        currentSpeed = Mathf.Clamp(currentSpeed, 0f, speedCap);
+        speedFactor *= decelerationFactor;
+        // currentSpeed = Mathf.Clamp(currentSpeed, 0f, speedCap);
     }
 
 
-    void CalculateSpeedBoost() {
+    Vector3 CalculateSpeedBoost() {
         // Calculate current controller velocity
         prev_pos = curr_pos;
         curr_pos = transform.position;
 
         prev_v = curr_v;
-        curr_v = new Vector3((curr_pos.x-prev_pos.x).Time.deltaTime, (curr_pos.y-prev_pos.y).Time.deltaTime, (curr_pos.z-prev_pos.z).Time.deltaTime);
+        curr_v = new Vector3((curr_pos.x-prev_pos.x)/Time.deltaTime, (curr_pos.y-prev_pos.y)/Time.deltaTime, (curr_pos.z-prev_pos.z)/Time.deltaTime);
 
         dv = new Vector3((curr_v.x - prev_v.x)/Time.deltaTime, (curr_v.y - prev_v.y)/Time.deltaTime, (curr_v.z - prev_v.z)/Time.deltaTime);
 
         Debug.Log(dv);
 
         // Flip current controller velocity to opposite direction
-        return new Vector3(-1*dv.x, -1*dv.y, -1*dv.z)
+        return new Vector3(-1*dv.x, -1*dv.y, -1*dv.z);
 
     }
 
     void ApplyBoost()
     {
         // Apply boost to character controller using acceleration
-        Vector3 move = boost_vel * Time.deltaTime;
+        speedFactor = 0;
+        Vector3 move = boost_vel * Time.deltaTime * speedFactor;
         characterController.Move(move);
     }
 }
