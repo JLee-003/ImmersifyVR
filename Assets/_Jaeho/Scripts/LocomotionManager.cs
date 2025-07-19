e89b4dd452be80e857c9a11033b0a084f8851811
Jaeho <jaeho5540@gmail.com>
2025-07-19 16:15:30 -0700

    fixed line renderer reference

--- Assets/_Jaeho/Scripts/LocomotionManager.cs
+++ Assets/_Jaeho/Scripts/LocomotionManager.cs
@@ -6,13 +6,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class LocomotionManager : MonoBehaviour
 {
     ActionBasedContinuousMoveProvider continuousMoveProvider;
-    Swimmer SwimmingEvaluator;
+    LineSwimmer SwimmingEvaluator;
     float waterWalkSpeed = 1.5f;
     float normalWalkSpeed = 3f;
     private void Start()
     {
         continuousMoveProvider = GetComponentInChildren<ActionBasedContinuousMoveProvider>();
-        SwimmingEvaluator = GetComponent<Swimmer>();
+        SwimmingEvaluator = GetComponent<LineSwimmer>();
 
         continuousMoveProvider.moveSpeed = normalWalkSpeed;
         SwimmingEvaluator.enabled = false;
