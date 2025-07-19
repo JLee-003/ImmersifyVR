288e79ee815bf1229250a1915038ea7d240dae63
codemaster-jojo <jonny.masterpi@gmail.com>
2025-07-19 14:10:13 -0700

    fixed line renderer
    forgot to push sorry

--- Assets/_Jaeho/Swimming/LineSwimmer.cs
+++ Assets/_Jaeho/Swimming/LineSwimmer.cs
@@ -16,12 +16,14 @@ public class LineSwimmer : MonoBehaviour
     [SerializeField] InputActionReference leftControllerSwimReference;
     [SerializeField] InputActionReference rightControllerSwimReference;
     [SerializeField] Transform trackingReference;
-    [SerializeField] Transform leftControllerTransform; // Reference to the left controller's transform
-    [SerializeField] Transform rightControllerTransform; // Reference to the right controller's transform
+    [SerializeField] Transform leftControllerTransform;
+    [SerializeField] Transform rightControllerTransform;
+
+    [Header("Line Renderers")]
+    [SerializeField] LineRenderer leftLineRenderer;
+    [SerializeField] LineRenderer rightLineRenderer;
 
     CharacterController characterController;
-    LineRenderer lineRenderer;
-
     float cooldownTimer;
     Vector3 velocity;
 
@@ -38,129 +40,127 @@ public class LineSwimmer : MonoBehaviour
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        lineRenderer = GetComponent<LineRenderer>();
 
-        lineRenderer.useWorldSpace = true;
-        lineRenderer.enabled = false;
+        SetupLineRenderer(leftLineRenderer);
+        SetupLineRenderer(rightLineRenderer);
     }
-    private void Update()
+
+    void Update()
     {
         LeftControllerSwim();
         RightControllerSwim();
 
-        UpdateStrokeLine();
+        UpdateStrokeLines();
 
         // Apply drag force
         if (velocity.sqrMagnitude > 0.01f)
         {
             velocity -= velocity * dragForce * Time.deltaTime;
         }
-
         else
         {
             velocity = Vector3.zero;
         }
 
-        // Move the character controller
         characterController.Move(velocity * Time.deltaTime);
     }
+
     void LeftControllerSwim()
     {
         cooldownTimer += Time.deltaTime;
 
-        //Left Grip Button Pressed
-        if (cooldownTimer > minTimeBetweenStrokes
-            && leftControllerSwimReference.action.IsPressed())
+        if (cooldownTimer > minTimeBetweenStrokes && leftControllerSwimReference.action.IsPressed())
         {
             if (!leftSwimStarted)
             {
                 startPosLeft = leftControllerTransform.localPosition;
             }
 
-            //Swimming haptics
-            HapticFeedbackManager.Instance.InitiateHapticFeedback(true, false, 0.1f, 0.1f);
-
+            HapticFeedbackManager.Instance?.InitiateHapticFeedback(true, false, 0.1f, 0.1f);
             leftSwimStarted = true;
         }
 
         if (leftSwimStarted && leftControllerSwimReference.action.WasReleasedThisFrame())
         {
             endPosLeft = leftControllerTransform.localPosition;
-
             leftActionVector = startPosLeft - endPosLeft;
 
-            // Perform force
             Vector3 worldVelocity = trackingReference.TransformDirection(leftActionVector);
             velocity += worldVelocity * swimForce;
 
             cooldownTimer = 0f;
-            //Swimming haptics
-            HapticFeedbackManager.Instance.InitiateHapticFeedback(true, false, 0.25f, 0.4f);
+            HapticFeedbackManager.Instance?.InitiateHapticFeedback(true, false, 0.25f, 0.4f);
             leftSwimStarted = false;
         }
     }
+
     void RightControllerSwim()
     {
         cooldownTimer += Time.deltaTime;
 
-        //Right Grip Button Pressed
-        if (cooldownTimer > minTimeBetweenStrokes
-            && rightControllerSwimReference.action.IsPressed())
+        if (cooldownTimer > minTimeBetweenStrokes && rightControllerSwimReference.action.IsPressed())
         {
             if (!rightSwimStarted)
             {
                 startPosRight = rightControllerTransform.localPosition;
             }
 
-            //Swimming haptics
-            HapticFeedbackManager.Instance.InitiateHapticFeedback(true, false, 0.1f, 0.1f);
-
+            HapticFeedbackManager.Instance?.InitiateHapticFeedback(true, false, 0.1f, 0.1f);
             rightSwimStarted = true;
         }
 
         if (rightSwimStarted && rightControllerSwimReference.action.WasReleasedThisFrame())
         {
             endPosRight = rightControllerTransform.localPosition;
-
             rightActionVector = startPosRight - endPosRight;
 
-            // Perform force
             Vector3 worldVelocity = trackingReference.TransformDirection(rightActionVector);
             velocity += worldVelocity * swimForce;
 
             cooldownTimer = 0f;
-            //Swimming haptics
-            HapticFeedbackManager.Instance.InitiateHapticFeedback(true, false, 0.25f, 0.4f);
+            HapticFeedbackManager.Instance?.InitiateHapticFeedback(true, false, 0.25f, 0.4f);
             rightSwimStarted = false;
         }
     }
-    void UpdateStrokeLine()
+
+    void UpdateStrokeLines()
     {
-        // Left stroke active = left controller line
         if (leftSwimStarted)
         {
-            lineRenderer.enabled = true;
-            lineRenderer.positionCount = 2;
-
-            lineRenderer.SetPosition(0, leftControllerTransform.parent.TransformPoint(startPosLeft));
-            lineRenderer.SetPosition(1, leftControllerTransform.position);
+            leftLineRenderer.enabled = true;
+            leftLineRenderer.positionCount = 2;
+            leftLineRenderer.SetPosition(0, leftControllerTransform.parent.TransformPoint(startPosLeft));
+            leftLineRenderer.SetPosition(1, leftControllerTransform.position);
         }
-
-        // Right stroke active = Right controller line
-        else if (rightSwimStarted)
-        {
-            lineRenderer.enabled = true;
-            lineRenderer.positionCount = 2;
-
-            lineRenderer.SetPosition(0, rightControllerTransform.parent.TransformPoint(startPosRight));
-            lineRenderer.SetPosition(1, rightControllerTransform.position);
-        }
-
-        // No stroke active = No line
         else
         {
-            lineRenderer.enabled = false;
+            leftLineRenderer.enabled = false;
+        }
+
+        if (rightSwimStarted)
+        {
+            rightLineRenderer.enabled = true;
+            rightLineRenderer.positionCount = 2;
+            rightLineRenderer.SetPosition(0, rightControllerTransform.parent.TransformPoint(startPosRight));
+            rightLineRenderer.SetPosition(1, rightControllerTransform.position);
+        }
+        else
+        {
+            rightLineRenderer.enabled = false;
         }
     }
-}
 
+    void SetupLineRenderer(LineRenderer lr)
+    {
+        if (lr == null) return;
+
+        lr.useWorldSpace = true;
+        lr.enabled = false;
+        lr.positionCount = 0;
+        lr.widthMultiplier = 0.01f;
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.startColor = new Color(0f, 0.8f, 1f); // light cyan
+        lr.endColor = new Color(0f, 0f, 1f);   // dark blue
+    }
+
+}
