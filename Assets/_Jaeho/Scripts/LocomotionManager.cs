27b50209015f3e82bbc2972d607602aa13b53600
Jaeho <jaeho5540@gmail.com>
2025-08-16 22:08:40 -0700

    reduced gravity

--- Assets/_Jaeho/Scripts/LocomotionManager.cs
+++ Assets/_Jaeho/Scripts/LocomotionManager.cs
@@ -23,7 +23,7 @@ public class LocomotionManager : MonoBehaviour
         {
             continuousMoveProvider.moveSpeed = waterWalkSpeed;
             SwimmingEvaluator.enabled = true;
-            Physics.gravity = new Vector3(0f, -0.01f, 0f);
+            Physics.gravity = new Vector3(0f, -0.0005f, 0f);
         }
     }
     private void OnTriggerExit(Collider other)
