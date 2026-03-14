d07b68f621771ddeddc7a4abd111f2d9403c8bdf
Jaeho <jaeho5540@gmail.com>
2026-03-14 01:51:02 -0700

    fine tuning/added left pickaxe

--- Assets/_Jaeho/Scripts/ForceGrab.cs
+++ Assets/_Jaeho/Scripts/ForceGrab.cs
@@ -6,16 +6,36 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ForceGrab : MonoBehaviour
 {
     XRGrabInteractable grabInteractable;
+    public bool isRight = true;
     private void Start()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
+        XRDirectInteractor rightInteractor = PlayerReferences.instance.rightController.GetComponentInChildren<XRDirectInteractor>();
+        XRDirectInteractor leftInteractor = PlayerReferences.instance.leftController.GetComponentInChildren<XRDirectInteractor>();
         //transform.position = PlayerReferences.instance.rightController.transform.position;
-        grabInteractable.interactionManager.SelectEnter(PlayerReferences.instance.rightController.GetComponentInChildren<XRDirectInteractor>() as IXRSelectInteractor, grabInteractable);
+        if (isRight)
+        {
+            grabInteractable.interactionManager.SelectEnter(rightInteractor as IXRSelectInteractor, grabInteractable);
+        }
+        else
+        {
+            grabInteractable.interactionManager.SelectEnter(leftInteractor as IXRSelectInteractor, grabInteractable);
+        }
+        
     }
     private void Update()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
+        XRDirectInteractor rightInteractor = PlayerReferences.instance.rightController.GetComponentInChildren<XRDirectInteractor>();
+        XRDirectInteractor leftInteractor = PlayerReferences.instance.leftController.GetComponentInChildren<XRDirectInteractor>();
         //transform.position = PlayerReferences.instance.rightController.transform.position;
-        grabInteractable.interactionManager.SelectEnter(PlayerReferences.instance.rightController.GetComponentInChildren<XRDirectInteractor>() as IXRSelectInteractor, grabInteractable);
+        if (isRight)
+        {
+            grabInteractable.interactionManager.SelectEnter(rightInteractor as IXRSelectInteractor, grabInteractable);
+        }
+        else
+        {
+            grabInteractable.interactionManager.SelectEnter(leftInteractor as IXRSelectInteractor, grabInteractable);
+        }
     }
 }
