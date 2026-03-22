1156e2763a9004f872d6f2dd630c88cb67973cbe
Jaeho <jaeho5540@gmail.com>
2026-03-21 23:16:32 -0700

    moved to dual scene, fixed gravity/engaged handoff, added text

--- Assets/_Jaeho/Scripts/PickaxeClimb.cs
+++ Assets/_Jaeho/Scripts/PickaxeClimb.cs
@@ -6,7 +6,7 @@ public class PickaxeClimb : MonoBehaviour
     XRGrabInteractable grabInteractable;
     Rigidbody rb;
 
-    [SerializeField] Transform controllerTarget;
+    Transform controllerTarget;
 
     Transform player;
     CharacterController characterController;
@@ -22,16 +22,23 @@ public class PickaxeClimb : MonoBehaviour
     [Header("Stability")]
     [SerializeField] float deadZone = 0.002f;
 
+    ForceGrab forceGrab;
+
     bool engaged;
     Transform weakPoint;
     RigidbodyConstraints originalConstraints;
     float originalStepOffset;
-    bool prevUseGravity;
 
     Vector3 currentControllerWorld;
     Vector3 lastControllerWorld;
     Vector3 engagedLocalController;
     bool hasSample;
+
+    public static PickaxeClimb engagedPickaxe = null;
+    public static PickaxeClimb leftPickaxe = null;
+    public static PickaxeClimb rightPickaxe = null;
+    public static bool rightEngaged = false;
+    public static bool leftEngaged = false;
 
     void Start()
     {
@@ -39,7 +46,21 @@ public class PickaxeClimb : MonoBehaviour
         grabInteractable = GetComponent<XRGrabInteractable>();
         originalConstraints = rb.constraints;
 
+        forceGrab = GetComponent<ForceGrab>();
+
         player = PlayerReferences.instance.playerObject.transform;
+
+        if (forceGrab.isRight)
+        {
+            controllerTarget = PlayerReferences.instance.rightController.transform;
+            rightPickaxe = this;
+        }
+        else
+        {
+            controllerTarget = PlayerReferences.instance.leftController.transform;
+            leftPickaxe = this;
+        }
+
         characterController = player.GetComponent<CharacterController>();
         moveProvider = player.GetComponentInChildren<ActionBasedContinuousMoveProvider>();
 
@@ -68,6 +89,9 @@ public class PickaxeClimb : MonoBehaviour
             return;
         }
 
+
+        if (engagedPickaxe != this) return;
+
         Vector3 controllerWorldDelta = currentControllerWorld - lastControllerWorld;
 
         Vector3 localDelta = weakPoint.InverseTransformVector(controllerWorldDelta);
@@ -77,7 +101,7 @@ public class PickaxeClimb : MonoBehaviour
             return;
 
         Vector3 move = -weakPoint.TransformVector(localDelta) * climbStrength;
-
+        
         if (move.magnitude > maxMovePerFixed)
             move = move.normalized * maxMovePerFixed;
 
@@ -86,6 +110,10 @@ public class PickaxeClimb : MonoBehaviour
         Vector3 actualRigMove = player.position - playerPosBefore;
 
         lastControllerWorld = currentControllerWorld + actualRigMove;
+    }
+    static bool AnyPickaxeEngaged()
+    {
+        return leftEngaged || rightEngaged;
     }
 
     public void TryEngage(Transform wp)
@@ -99,6 +127,18 @@ public class PickaxeClimb : MonoBehaviour
     {
         engaged = true;
 
+        if (forceGrab.isRight)
+        {
+            rightEngaged = true;
+        }
+
+        else
+        {
+            leftEngaged = true;
+        }
+
+        engagedPickaxe = this;
+
         grabInteractable.trackPosition = false;
         grabInteractable.trackRotation = false;
 
@@ -108,35 +148,69 @@ public class PickaxeClimb : MonoBehaviour
 
         if (moveProvider)
         {
-            prevUseGravity = moveProvider.useGravity;
             moveProvider.useGravity = false;
         }
 
         if (characterController)
+        {
             characterController.stepOffset = 0f;
+        }
 
         currentControllerWorld = controllerTarget.position;
         lastControllerWorld = currentControllerWorld;
         engagedLocalController = weakPoint.InverseTransformPoint(currentControllerWorld);
         hasSample = true;
+
+        HapticFeedbackManager.Instance?.InitiateHapticFeedback(!forceGrab.isRight, forceGrab.isRight, 1.0f, 0.5f);
     }
 
     public void Disengage()
     {
         engaged = false;
 
+        if (forceGrab.isRight)
+        {
+            rightEngaged = false;
+        }
+        else
+        {
+            leftEngaged = false;
+        }
+
+        // hand off movement control if the other pickaxe is still engaged
+        if (engagedPickaxe == this)
+        {
+            if (forceGrab.isRight)
+            {
+                engagedPickaxe = (leftPickaxe != null && leftPickaxe.engaged) ? leftPickaxe : null;
+            }
+            else
+            {
+                engagedPickaxe = (rightPickaxe != null && rightPickaxe.engaged) ? rightPickaxe : null;
+            }
+        }
+
         grabInteractable.trackPosition = true;
         grabInteractable.trackRotation = true;
 
         rb.constraints = originalConstraints;
 
-        if (moveProvider)
-            moveProvider.useGravity = prevUseGravity;
+        if (!AnyPickaxeEngaged())
+        {
+            if (moveProvider)
+            {
+                moveProvider.useGravity = true;
+            }
 
-        if (characterController)
-            characterController.stepOffset = originalStepOffset;
+            if (characterController)
+            {
+                characterController.stepOffset = originalStepOffset;
+            }
+        }
 
         weakPoint = null;
         hasSample = false;
+
+        HapticFeedbackManager.Instance?.InitiateHapticFeedback(!forceGrab.isRight, forceGrab.isRight, 0.7f, 0.1f);
     }
 }
