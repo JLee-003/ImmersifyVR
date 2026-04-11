6792475bdf4d3aa83e22574454aa76f394f83d26
Jaeho <jaeho5540@gmail.com>
2026-04-11 12:36:48 -0700

    made rock break slowly not one hit

--- Assets/_Jaeho/Swimming/BreakableRock.cs
+++ Assets/_Jaeho/Swimming/BreakableRock.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class BreakableRock : MonoBehaviour
 {
     LineSwimmer swimmer;
+    float velocityRequired = 8f;
+    float velocityTaken = 0f;
+
+    Renderer rend;
+    Color originalColor;
+    Color crackedRock = new Color(0.9f, 0.87f, 0.82f);
     private void Start()
     {
         swimmer = PlayerReferences.instance.GetComponent<LineSwimmer>();
+        rend = GetComponent<Renderer>();
+        originalColor = rend.material.color;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Rock Broken");
+            Debug.Log("Rock Hit with velocity " + swimmer.velocity.magnitude);
+            velocityTaken += swimmer.velocity.magnitude;
             swimmer.velocity *= -0.5f;
-            Destroy(gameObject);
+
+            float t = Mathf.Clamp01(velocityTaken / velocityRequired);
+            rend.material.color = Color.Lerp(originalColor, crackedRock, t);
+
+            if (velocityTaken >= velocityRequired)
+            {
+                Destroy(gameObject);
+            }
         }
         else
         {
